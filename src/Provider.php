fd8978c45ac142932d27b818e1c645a4d2705d2a
Justin Tadlock <justintadlock@gmail.com>
2021-06-22 22:46:14 -0500

    Separate `hybrid-template-hierarchy` into its own repository.

---
+++ src/Provider.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Template hierarchy service provider.
+ *
+ * This is the service provider for the template hierarchy. It's used to register
+ * the template hierarchy with the container and boot it when needed.
+ *
+ * @package   HybridCore
+ * @author    Justin Tadlock <justintadlock@gmail.com>
+ * @copyright Copyright (c) 2008 - 2021, Justin Tadlock
+ * @link      https://themehybrid.com/hybrid-core
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+namespace Hybrid\Template\Hierarchy;
+
+use Hybrid\Template\Contracts\Hierarchy;
+use Hybrid\Tools\ServiceProvider;
+
+/**
+ * Template hierarchy provider class.
+ *
+ * @since  1.0.0
+ * @access public
+ */
+class Provider extends ServiceProvider {
+
+	/**
+	 * Registration callback that adds a single instance of the template
+	 * hierarchy to the container.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function register() {
+
+		$this->app->singleton( Hierarchy::class, Component::class );
+
+		$this->app->alias( Hierarchy::class, 'template/hierarchy' );
+	}
+
+	/**
+	 * Boots the hierarchy by firing its hooks in the `boot()` method.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function boot() {
+
+		$this->app->resolve( Hierarchy::class )->boot();
+	}
+}
